b753dbaaf611ed659dfcbb80e8afc27aa5ce1a88
willy <willy8742891@gmail.com>
2022-05-04 15:08:10 +0800

    add git ops.

--- src/git/index.ts
+++ src/git/index.ts
@@ -1,16 +1,45 @@
 import simpleGit from "simple-git";
 import { ToolOptions } from "@/types/options";
+import path from "path";
+import fs from "fs";
 
-export default function git(
+export default async function (
   command: Array<string | number>,
   options: Partial<ToolOptions>
-): void {
+): Promise<void> {
   const gitCommand = command.slice(1).map(String);
-  const root = options.root;
+  const root = options.root || process.cwd();
   const gitConfig = options?.git || {};
   const config = gitConfig?.config || [];
   const git = simpleGit(root, { config });
+
   if (gitCommand[0] === "add") {
-    git.add(gitCommand.slice(1));
+    try {
+      git.add(gitCommand.slice(1));
+    } catch (error) {
+      console.log("Error");
+    }
   }
+
+  if (gitCommand[0] === "commit") {
+    try {
+      git.commit(gitCommand[1]);
+    } catch (error) {
+      console.log("Error");
+    }
+  }
+
+  if (gitCommand[0] === "log") {
+    console.log(await git.log());
+  }
+
+  const logData = await git.log();
+  fs.promises.writeFile(
+    path.join(root, "logs", "git-logs.json"),
+    JSON.stringify(logData.all)
+  );
+  fs.promises.writeFile(
+    path.join(root, "logs", "git-logs-latest.json"),
+    JSON.stringify(logData.latest)
+  );
 }
--- tool.config.ts
+++ tool.config.ts
@@ -3,6 +3,6 @@ import tool from "./main";
 tool({
   root: process.cwd(),
   git: {
-    config: ["user.name=willy874", "user.email=willy874@gmail.com"],
+    config: ["user.name=willy874", "user.email=willy8742891@gmail.com"],
   },
 });
